ffb7df0458f44ce43ef6d0158bb06e13f5a6b962
Franco-Olmos <5749@holbertonstudents.com>
2022-09-29 18:35:49 -0700

    task3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always (0) Success
+ */
+int main(void)
+{
+        char chars;
+
+        for (chars = 97; chars <= 122; chars++)
+                putchar(chars);
+        for (chars = 65; chars <= 90; chars++)
+                putchar(chars);
+        putchar('\n');
+        return (0);
+}

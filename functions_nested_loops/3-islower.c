aa02579820876f4882a57413847663cccd521243
Franco-Olmos <5749@holbertonstudents.com>
2022-10-03 06:30:01 -0700

    bett3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - checks for lowercase characters
  * @c: Stores the ascii code of character to be checked
- * Return: 1 if c is lowercase, return 0 otherwise 
+ * Return: 1 if c is lowercase, return 0 otherwise
  */
 int _islower(int c)
 {

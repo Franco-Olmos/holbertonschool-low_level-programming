6fd02cec7fc93e25691ad832d4189eea442f6012
Franco-Olmos <5749@holbertonstudents.com>
2022-10-12 19:10:21 -0700

    tasks

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* first project */ 
 int _putchar(char);
 void reset_to_98(int *);
 void swap_int(int *, int *);
@@ -9,3 +17,14 @@ void puts2(char *);
 void puts_half(char *);
 void print_array(int *, int);
 char *_strcpy(char *, char *);
+/* second project */
+char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+int _atoi(char *s);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+
+#endif

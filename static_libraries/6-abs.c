08676cb49237298e8b6e6c6aca58eaab567ac8ca
Franco-Olmos <5749@holbertonstudents.com>
2022-10-17 08:25:51 -0700

    t0

--- static_libraries/6-abs.c
+++ static_libraries/6-abs.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  * _abs - Computes the absolute value of an integer.
- * @r: The integer to be computed.
+ * @n: The integer to be computed.
  *
  * Return: The absolute value of the integer.
  */
-int _abs(int r)
+int _abs(int n)
 
 {
-	if (r >= 0)
+	if (n >= 0)
 	{
-		return (r);
+		return (n);
 	}
 	else
 	{
-		return (-r);
+		return (-n);
 	}
 }

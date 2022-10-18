0859851c1fa248afe0c25276fb9f32fa9af51958
Franco-Olmos <5749@holbertonstudents.com>
2022-10-18 13:52:10 -0700

    t3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiply two numbers
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc < 3)
+	{
+		printf("Error");
+		printf("\n");
+		return (1);
+	}
+	else
+	{
+		i = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d", i);
+		printf("\n");
+		return (0);
+	}
+
+}

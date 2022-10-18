d801dcbbe964a8e6dd6b7d7b92c6466a49b23949
Franco-Olmos <5749@holbertonstudents.com>
2022-10-18 04:02:58 -0700

    remotepush

---
+++ recursion/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*libraries*/
+#include <unistd.h>
+
+/*functions*/
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt_recursion2(int i, int k);
+int is_prime_number(int n);
+int is_prime_number2(int i, int n);
+
+#endif
+
+

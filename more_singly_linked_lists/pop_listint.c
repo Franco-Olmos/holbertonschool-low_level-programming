07335a2f77937e70a7483deec9683a00e2bfdfb2
Franco-Olmos <5749@holbertonstudents.com>
2022-11-20 10:19:00 -0800

    pop_listint push

---
+++ more_singly_linked_lists/pop_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * _pop_listint - deletes the head node
+ * @head: pointer to the first node
+ *
+ * Return: head node’s data (n)
+ */
+int _pop_listint(listint_t **head)
+{
+	listint_t *node = *head;
+
+	if (!head || !*head)
+		return (-1);
+	*head = node->next;
+	free(node);
+	return (1);
+}

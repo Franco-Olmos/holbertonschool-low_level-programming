304574a3cf57d7a3fa124c300c1f7057e05a7fe8
Franco-Olmos <5749@holbertonstudents.com>
2022-11-26 19:47:41 -0800

    file io

---
+++ file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - create file with certain content
+ * @filename: pointer to file
+ * @text_content: letters to add to file
+ * Return: actual number of letters or 0
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fp = 0, st = 0, wr = 0;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+	{
+		fp = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+		return (1);
+	}
+	fp = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	if (fp == -1)
+		return (-1);
+	st = strlen(text_content);
+	wr = write(fp, text_content, st);
+	if (wr == -1)
+		return (0);
+	close(fp);
+	return (1);
+}
